3d9fc22d5d16c2cfffd318a8d6f841ad13528188
staregee <112976171+staregee@users.noreply.github.com>
2023-08-22 12:15:34 +0300

    Add files via upload

---
+++ conveyor2.ino
@@ -0,0 +1,124 @@
+#include <Servo.h>
+
+Servo dropServo;
+
+#define S0 4
+#define S1 5
+#define S2 7
+#define S3 6
+#define sensorOut 8
+#define in3 9
+#define in4 10
+#define en2 11
+#define FORWARD 3
+#define BACK 2
+
+
+
+unsigned char Forward_Speed = 140;
+unsigned char Back_Speed = 100;
+
+
+int GetColor() {
+  int frequency = 0;
+  int sortColor = 0;
+  int color = 0;
+
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  frequency = pulseIn(sensorOut, LOW);
+  int R = frequency;
+  Serial.print("Red = ");
+  Serial.print(frequency);
+  Serial.print("   ");
+  delay(50);
+
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  frequency = pulseIn(sensorOut, LOW);
+  int B = frequency;
+  Serial.print("Blue = ");
+  Serial.print(frequency);
+  Serial.println("   ");
+  delay(50);
+
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  frequency = pulseIn(sensorOut, LOW);
+  int G = frequency;
+  Serial.print("Green = ");
+  Serial.print(frequency);
+  Serial.print("   ");
+  delay(50);
+
+  if (R < 50) {
+   dropServo.write(0);
+  } else if (B < 70) {
+     dropServo.write(90);
+  } else {
+//    color = 0;
+  }
+  return color;
+}
+
+void setup()
+{
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(FORWARD, OUTPUT);
+  pinMode(BACK, OUTPUT);
+
+
+  pinMode(sensorOut, INPUT);
+
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
+ dropServo.attach(9);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  analogWrite(FORWARD, Forward_Speed);
+  
+  GetColor();
+
+
+
+
+
+  
+  
+//  while (true) {
+//   
+//    while (Serial.available() == 0);
+//    
+//    char data = Serial.read();
+//    if (data == '1') {
+//      break;
+//    } else if (data == '2') {
+//      sortColor = RED;
+//    } else {
+//     
+//    }
+//  }
+//
+//  while (GetColor() == 0) {
+//
+//  }
+//
+//  if (GetColor() == sortColor) {
+//    dropServo.write(20);
+//  } else {
+//    dropServo.write(0);
+//  }
+
+
+}
